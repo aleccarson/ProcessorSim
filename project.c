bf53ee6d25c4ce4ed0dc03ca96e108621b61af6c
aleccarson <aleccarsonn@gmail.com>
2020-04-19 13:41:26 -0400

    99% complete,  not updating properly

--- project.c
+++ project.c
@@ -83,7 +83,7 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
     // 31-26(op)
     *op = (instruction & 0xfc000000) >> 26;
     // 25-21 (r1)
-    *r1 = (instruction & 0x03e00000) >> 32;
+    *r1 = (instruction & 0x03e00000) >> 21;
     // 20-16 (r2)
     *r2 = (instruction & 0x001f0000) >> 16;
     // 15-11 (r3)
@@ -102,140 +102,147 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
 /* 15 Points */
 int instruction_decode(unsigned op,struct_controls *controls)
 {   
-    // REgister type
-    if(op == 0)
+    switch(op)
     {
-        controls->MemtoReg = 0;
-        controls->MemRead = 0;
-        controls->MemWrite = 0;
-        controls->ALUSrc = 1;
-        controls->ALUOp = 7;
-        controls->Jump = 0;
-        controls->Branch = 0;
-        controls->RegDst = 1;
-        controls->RegWrite = 1;
+        default:
+            return 1;
+
+        // REgister type
+        case 0:
+            controls->MemtoReg = 0;
+            controls->MemRead = 0;
+            controls->MemWrite = 0;
+            controls->ALUSrc = 0;
+            controls->ALUOp = 7;
+            controls->Jump = 0;
+            controls->Branch = 0;
+            controls->RegDst = 1;
+            controls->RegWrite = 1;
+
+            break;
+        
+        // Immediate type (beq)
+        case 4:
+            controls->MemtoReg = 2;
+            controls->MemRead = 0;
+            controls->MemWrite = 0;
+            controls->ALUSrc = 0;
+            controls->ALUOp = 1;
+            controls->Jump = 0;
+            controls->Branch = 1;
+            controls->RegDst = 2;
+            controls->RegWrite = 0;
+
+            break;
+        
+        // immediate type (addi)
+        case 8:
+            controls->MemtoReg = 0;
+            controls->MemRead = 0;
+            controls->MemWrite = 0;
+            controls->ALUSrc = 1;
+            controls->ALUOp = 0;
+            controls->Jump = 0;
+            controls->Branch = 0;
+            controls->RegDst = 0;
+            controls->RegWrite = 1;
+
+            break;
+    
+        //immediate type slt
+        case 10:
+            controls->MemtoReg = 0;
+            controls->MemRead = 0;
+            controls->MemWrite = 0;
+            controls->ALUSrc = 0;
+            controls->ALUOp = 2;
+            controls->Jump = 0;
+            controls->Branch = 0;
+            controls->RegDst = 1;
+            controls->RegWrite = 1;
+
+            break;
+
+        //immediate tyope (sltu)
+        case 11:
+            controls->MemtoReg = 0;
+            controls->MemRead = 0;
+            controls->MemWrite = 0;
+            controls->ALUSrc = 0;
+            controls->ALUOp = 3;
+            controls->Jump = 0;
+            controls->Branch = 0;
+            controls->RegDst = 1;
+            controls->RegWrite = 1;
+
+            break;
+
+        //immediate type lui
+        case 15:
+            controls->MemtoReg = 0;
+            controls->MemRead = 0;
+            controls->MemWrite = 0;
+            controls->ALUSrc = 1;
+            controls->ALUOp = 6;
+            controls->Jump = 0;
+            controls->Branch = 0;
+            controls->RegDst = 0;
+            controls->RegWrite = 1;
+
+            break;
+    
+        //immediate type lw
+        case 35:
+            controls->MemtoReg = 1;
+            controls->MemRead = 1;
+            controls->MemWrite = 0;
+            controls->ALUSrc = 1;
+            controls->ALUOp = 0;
+            controls->Jump = 0;
+            controls->Branch = 0;
+            controls->RegDst = 0;
+            controls->RegWrite = 1;
+
+            break;
+
+        //immediate type sw
+        case 43:
+            controls->MemtoReg = 2;
+            controls->MemRead = 0;
+            controls->MemWrite = 1;
+            controls->ALUSrc = 1;
+            controls->ALUOp = 0;
+            controls->Jump = 0;
+            controls->Branch = 0;
+            controls->RegDst = 2;
+            controls->RegWrite = 0;
+
+            break;
+        
+        // J type (jump)
+        case 2:
+            controls->MemtoReg = 0;
+            controls->MemRead = 0;
+            controls->MemWrite = 0;
+            controls->ALUSrc = 0;
+            controls->ALUOp = 0;
+            controls->Jump = 1;
+            controls->Branch = 0;
+            controls->RegDst = 0;
+            controls->RegWrite = 0;
+
+            break;
     }
 
-    // Immediate type (beq)
-    if(op == 4)
-    {
-        controls->MemtoReg = 2;
-        controls->MemRead = 0;
-        controls->MemWrite = 0;
-        controls->ALUSrc = 0;
-        controls->ALUOp = 1;
-        controls->Jump = 0;
-        controls->Branch = 1;
-        controls->RegDst = 2;
-        controls->RegWrite = 0;
-    }
-
-    // immediate type (addi)
-    if(op == 8)
-    {
-        controls->MemtoReg = 0;
-        controls->MemRead = 0;
-        controls->MemWrite = 0;
-        controls->ALUSrc = 1;
-        controls->ALUOp = 0;
-        controls->Jump = 0;
-        controls->Branch = 0;
-        controls->RegDst = 0;
-        controls->RegWrite = 1;
-    }
-
-    //immediate type slt
-    if(op == 10)
-    {
-        controls->MemtoReg = 0;
-        controls->MemRead = 0;
-        controls->MemWrite = 0;
-        controls->ALUSrc = 0;
-        controls->ALUOp = 2;
-        controls->Jump = 0;
-        controls->Branch = 0;
-        controls->RegDst = 1;
-        controls->RegWrite = 1;
-    }
-
-    //immediate tyope (sltu)
-    if(op == 11)
-    {
-        controls->MemtoReg = 0;
-        controls->MemRead = 0;
-        controls->MemWrite = 0;
-        controls->ALUSrc = 0;
-        controls->ALUOp = 3;
-        controls->Jump = 0;
-        controls->Branch = 0;
-        controls->RegDst = 1;
-        controls->RegWrite = 1;
-    }
-
-    //immediate type lui
-    if(op == 15)
-    {
-        controls->MemtoReg = 0;
-        controls->MemRead = 0;
-        controls->MemWrite = 0;
-        controls->ALUSrc = 1;
-        controls->ALUOp = 6;
-        controls->Jump = 0;
-        controls->Branch = 0;
-        controls->RegDst = 0;
-        controls->RegWrite = 1;
-    }
-
-    //immediate type lw
-    if(op == 35)
-    {
-        controls->MemtoReg = 1;
-        controls->MemRead = 1;
-        controls->MemWrite = 0;
-        controls->ALUSrc = 1;
-        controls->ALUOp = 0;
-        controls->Jump = 0;
-        controls->Branch = 0;
-        controls->RegDst = 0;
-        controls->RegWrite = 1;
-    }
-
-    //immediate type sw
-    if(op == 43)
-    {
-        controls->MemtoReg = 2;
-        controls->MemRead = 0;
-        controls->MemWrite = 1;
-        controls->ALUSrc = 1;
-        controls->ALUOp = 0;
-        controls->Jump = 0;
-        controls->Branch = 0;
-        controls->RegDst = 2;
-        controls->RegWrite = 0;
-    }
-
-    // J type (jump)
-    if(op == 2)
-    {
-        controls->MemtoReg = 0;
-        controls->MemRead = 0;
-        controls->MemWrite = 0;
-        controls->ALUSrc = 0;
-        controls->ALUOp = 0;
-        controls->Jump = 1;
-        controls->Branch = 0;
-        controls->RegDst = 0;
-        controls->RegWrite = 0;
-    }
-    // halt if invalid opcode
-    return 1;
+    return 0;
 }
 
 /* Read Register */
 /* 5 Points */
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
 {
-
+    // get values from reg array 
+    *data1 = Reg[r1], *data2 = Reg[r2];
 }
 
 
@@ -243,21 +250,92 @@ void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigne
 /* 10 Points */
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
+    // check if 16th bit is 1 meaning it's neg (extend 1s)
+    if(offset >> 15 & 1)
+        *extended_value = offset | 0xffff0000;
 
+    // extend 0s otherwise
+    *extended_value = offset & 0x0000ffff;
 }
 
 /* ALU operations */
 /* 10 Points */
 int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
 {
+    // I type ops
+    if(ALUSrc == 1)
+        ALU(data1, extended_value, ALUOp, ALUresult, Zero);
 
+    if(ALUSrc == 0)
+    {
+    // r type instructions'
+        switch(funct)
+        {
+            // halt/don't care
+            default:
+                return 1;
+
+            //addition
+            case 32:
+                ALUOp = 0;
+                break;
+            
+            // sbutraction
+            case 34:
+                ALUOp = 1;
+                break;
+
+            // slt signed
+            case 42:
+                ALUOp = 2;
+                break;
+
+            //slt unsigned
+            case 43:
+                ALUOp = 3;
+                break;
+
+            // bitwise AND
+            case 36:
+                ALUOp = 4;
+                break;
+            
+            //bitwise OR
+            case 37:
+                ALUOp = 5;
+                break;
+
+            // slr  
+            case 4:
+                ALUOp = 6;
+                break;
+
+            // bitwie nor
+            case 39:
+                ALUOp = 7;
+                break;
+
+        }
+        //ALU function
+        ALU(data1, data2, ALUOp, ALUresult, Zero);
+    }
+    return 0;
 }
 
 /* Read / Write Memory */
 /* 10 Points */
 int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
 {
+  if((MemRead == 1 || MemWrite == 1) && ALUresult % 4 != 0)
+    return 1;
 
+    if(MemRead == 1)
+        *memdata = Mem[ALUresult >> 2];
+    
+    if(MemWrite == 1)
+        Mem[ALUresult >> 2] = data2;
+
+    return 0;
 }
 
 
@@ -265,13 +343,37 @@ int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsig
 /* 10 Points */
 void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
 {
+        if(RegWrite == 1)
+        {
+            // to r3
+            if(MemtoReg == 1 && RegDst == 1)
+                Reg[r3] = memdata;
 
+            // mem to reg
+            if(MemtoReg == 1 && RegDst == 0)
+                Reg[r2] = memdata;
+
+            // store in r2
+            if(MemtoReg == 0 && RegDst == 0)
+                Reg[r2] = ALUresult;
+
+            // store in r3
+            if(MemtoReg == 0 && RegDst == 1)
+                Reg[r3] = ALUresult;
+        }
 }
 
 /* PC update */
 /* 10 Points */
 void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
 {
+    // pc inc by 4 after each instruction
+    *PC += 4;
+    //branching and got 0
+    if(Zero == 1 && Branch == 1)
+        *PC += extended_value << 2;
 
+    if(Jump == 1)
+        *PC = (jsec << 2) | (*PC | 0xf0000000);
 }
 
